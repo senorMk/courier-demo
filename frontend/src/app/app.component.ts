b6c08e14806d7f854b7ed27ec21d9bf3c91717c0
PenjaniMLupiya <penjani.mkandawire@lupiya.com>
2025-08-16 20:46:59 +0200

    Client project template setup

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [RouterOutlet],
 })
 export class AppComponent {
-  title = 'platinum-web';
+    /**
+     * Constructor
+     */
+    constructor() {}
 }
