2c4783872d6270abbd4834eb6d01dd62d9caa183
PenjaniMLupiya <penjani.mkandawire@lupiya.com>
2025-08-18 11:53:18 +0200

    feat: add environment configuration files for production and QA

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,13 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  hmr: false,
+  serverURL: "http://localhost:3000/api",
+  apiURL: "http://localhost:3000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
