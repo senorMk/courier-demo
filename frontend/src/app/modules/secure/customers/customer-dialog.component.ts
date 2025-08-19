5e8a0b580026b1268f04a2bc653550ee866b4fa5
PenjaniMLupiya <penjani.mkandawire@lupiya.com>
2025-08-19 22:22:21 +0200

    feat(frontend): enhance user and customer dialogs with improved styling, validation, and feedback mechanisms

--- frontend/src/app/modules/secure/customers/customer-dialog.component.ts
+++ frontend/src/app/modules/secure/customers/customer-dialog.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
-import { CustomersService } from './customers.service';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatDialogModule, MatDialogRef } from "@angular/material/dialog";
+import { CustomersService } from "./customers.service";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSnackBar, MatSnackBarModule } from "@angular/material/snack-bar";
 
 @Component({
-  selector: 'app-customer-dialog',
-  templateUrl: './customer-dialog.component.html',
+  selector: "app-customer-dialog",
+  templateUrl: "./customer-dialog.component.html",
   standalone: true,
   imports: [
     MatFormFieldModule,
-    // ...other modules if needed...
-  ReactiveFormsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatSnackBarModule,
   ],
 })
 export class CustomerDialogComponent {
@@ -22,14 +28,15 @@ export class CustomerDialogComponent {
   constructor(
     private _fb: FormBuilder,
     private _service: CustomersService,
-    private _dialogRef: MatDialogRef<CustomerDialogComponent>
+    private _dialogRef: MatDialogRef<CustomerDialogComponent>,
+    private _snackBar: MatSnackBar
   ) {
     this.form = this._fb.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      phoneNumber: ['', Validators.required],
-      emailAddress: [''],
-      idNumber: ['']
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      phoneNumber: ["", Validators.required],
+      emailAddress: [""],
+      idNumber: [""],
     });
   }
 
@@ -41,11 +48,23 @@ export class CustomerDialogComponent {
     this._service.createCustomer(this.form.value).subscribe({
       next: () => {
         this.loading = false;
+        this._snackBar.open("Customer created successfully!", "Close", {
+          duration: 3000,
+          verticalPosition: "top",
+        });
         this._dialogRef.close(true);
       },
       error: () => {
         this.loading = false;
-      }
+        this._snackBar.open(
+          "Failed to create customer. Please try again.",
+          "Close",
+          {
+            duration: 4000,
+            verticalPosition: "top",
+          }
+        );
+      },
     });
   }
 }
--- frontend/src/app/modules/secure/customers/customers.component.ts
+++ frontend/src/app/modules/secure/customers/customers.component.ts
@@ -1,28 +1,37 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialog } from '@angular/material/dialog';
-import { CustomersService, Customer } from './customers.service';
-import { CustomerDialogComponent } from './customer-dialog.component';
-import { MatTableModule } from '@angular/material/table';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
+import { MatDialog } from "@angular/material/dialog";
+import { CustomersService, Customer } from "./customers.service";
+import { CustomerDialogComponent } from "./customer-dialog.component";
+import { MatTableModule } from "@angular/material/table";
+import { MatIconModule } from "@angular/material/icon";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-customers',
-  templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.scss'],
+  selector: "app-customers",
+  templateUrl: "./customers.component.html",
+  styleUrls: ["./customers.component.scss"],
   standalone: true,
-  imports: [
-    MatPaginatorModule,
-    MatTableModule,
-  ],
+  imports: [MatPaginatorModule, MatTableModule, MatIconModule, CommonModule],
 })
 export class CustomersComponent implements OnInit {
-  displayedColumns: string[] = ['firstName', 'lastName', 'phoneNumber', 'emailAddress', 'idNumber'];
+  displayedColumns: string[] = [
+    "firstName",
+    "lastName",
+    "phoneNumber",
+    "emailAddress",
+    "idNumber",
+    "createdAt"
+  ];
   dataSource = new MatTableDataSource<Customer>([]);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private _service: CustomersService, private _dialog: MatDialog) {}
+  constructor(
+    private _service: CustomersService,
+    private _dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.loadData();
@@ -39,7 +48,9 @@ export class CustomersComponent implements OnInit {
   }
 
   openCreateDialog(): void {
-    const dialogRef = this._dialog.open(CustomerDialogComponent);
+    const dialogRef = this._dialog.open(CustomerDialogComponent, {
+      width: "400px",
+    });
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
         this.loadData();
@@ -47,4 +58,3 @@ export class CustomersComponent implements OnInit {
     });
   }
 }
-
