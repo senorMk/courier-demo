602a059a8f94234073c5b66bec38c0b903136f33
Archie Nshindano <archienshindano@gmail.com>
2025-08-19 07:29:02 +0200

    Restoring some lines as original

--- frontend/src/app/modules/secure/dashboard/dashboard.component.ts
+++ frontend/src/app/modules/secure/dashboard/dashboard.component.ts
@@ -1,9 +1,17 @@
+
+import { MatDialog } from "@angular/material/dialog";
+import { ApexOptions } from "ng-apexcharts";
+
+import { Platform } from "@angular/cdk/platform";
+import { UserService } from "app/core/user/user.service";
+import { DashboardService } from "./dashboard.service";
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { FuseNavigationService, FuseVerticalNavigationComponent } from '@fuse/components/navigation';
 import { NavigationService } from 'app/core/navigation/navigation.service';
 import { Navigation } from 'app/core/navigation/navigation.types';
+
 
 @Component({
     selector   : 'administrator-dashboard',
@@ -12,24 +20,35 @@ import { Navigation } from 'app/core/navigation/navigation.types';
 })
 export class DashboardComponent implements OnInit, OnDestroy
 {
+
+    loggedInUser;
     navigation: Navigation;
     private _unsubscribeAll: Subject<any> = new Subject<any>();
+    UserService: UserService;
 
     constructor(
         private _fuseNavigationService: FuseNavigationService,
         private _navigationService: NavigationService,
+        private _userService: UserService,
+      
     )
     {
     }
 
     ngOnInit(): void
     {
-        this._navigationService.navigation$
+        
+      let lastLogin = this.loggedInUser.lastLogin;
+
+      
+
+      this._navigationService.navigation$
             .pipe(takeUntil(this._unsubscribeAll))
             .subscribe((navigation: Navigation) =>
             {
                 this.navigation = navigation;
-            });
+            }
+          );
     }
 
     ngOnDestroy(): void
